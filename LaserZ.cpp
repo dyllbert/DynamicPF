912bb05460ed366159b1692d421bb4da032af39b
Joseph Miera <jmiera@byu.edu>
2020-12-08 00:02:22 -0700

    Put in temporary fixes

--- LaserZ.cpp
+++ LaserZ.cpp
@@ -29,7 +29,7 @@ double LaserZ::getLaserAngle(int laser_index) {
 }
 
 double LaserZ::getLaserCount() {
-    return laser_angles.size();
+    return 21;//laser_angles.size();
 }
 
 void LaserZ::setLaserAngles(std::vector<double> angles) {
@@ -37,36 +37,8 @@ void LaserZ::setLaserAngles(std::vector<double> angles) {
     for (int i = 0; i < 21; i++) {
         laser_angles[i] = angles[i];
     }
-    for (int i = 0; i < 21; i++) {
-        laser_angles[i] = angles[i];
-    }
-    for (int i = 0; i < 21; i++) {
-        laser_angles[i] = angles[i];
-    }
 }
 
 int LaserZ::isInitialized() {
-    // if (laser_angles.size() == 0) {
-    //     return 0;
-    // }
-    for (int i = 0; i < 21; i++) {
-        laser_angles[i] = angles[i];
-    }
-}
-// 
-// int // LaserZ::isInitialized() {
-}
-
-int LaserZ::isInitialized() {
-    // if (laser_angles.size() == 0) {
-    //     return 0;
-    // }
     return 1;
-}
-
-double LaserZ::laserRangeModel(double z, double z_exp) {
-    double a_norm = 0.7;
-    double a_max = 0.3;
-    if (z > LASER_MAX_RANGE) {return a_max;}
-    else {return a_norm * 1/sqrt(2.0 * M_PI * LASER_STD_DEV) * exp(-(pow(z-z_exp, 2)) / (2 * LASER_STD_DEV));}
 }
